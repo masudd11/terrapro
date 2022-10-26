09b2686754772459edfb6c157da7edfc99469b4e
masudd11 <waju786shaikh@gmail.com>
2022-10-26 16:25:09 +0530

    first commit

---
+++ terraform.tfvars
@@ -0,0 +1,3 @@
+instance_type = "t2.micro"
+image_id      = "ami-01216e7612243e0ef"
+key_name      = "test1-key"
---
+++ variables.tf
@@ -0,0 +1,11 @@
+variable "instance_type" {
+  type = string
+}
+
+variable "image_id" {
+  type = string
+}
+
+variable "key_name" {
+  type = string
+}
